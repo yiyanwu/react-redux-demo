df2dc61f80664fe6bf8ecf0e3ff72f851d118407
wuyiyan <308111394@qq.com>
2019-04-19 15:58:23 +0800

    簡單的加一減一功能

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { connect } from 'react-redux';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        你点击了<span id="value">{this.props.n}</span>次
+        <div>
+        <button id="add1" onClick={()=>this.props.add1()}>加1</button>
+        <button id="minus1" onClick={()=>this.props.minus1()}>減1</button>
+        </div>
       </div>
     );
   }
 }
 
-export default App;
+function mapStateToProps(state){//獲取的是部分的數據，比如n 必須是個函數
+  return{
+    n: state.n
+  }
+}
+
+function mapDispatchToProps (dispatch){//創造action 
+  return{ 
+    add1: ()=>dispatch ({type:'add',payload:1}),
+    minus1: ()=>dispatch ({type:'minus',payload:1})
+  }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(App);//返回的是一個connect之後的App
--- src/index.js
+++ src/index.js
@@ -3,10 +3,39 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const stateChanger = (state, action) => {//控制state的操作
+    if (state === undefined) {
+        return {n: 0}//n方便獲取
+    } else {
+        if (action.type === 'add') {
+            var newState = {n:state.n + action.payload}
+            return newState
+        }else if(action.type === 'minus'){
+            var newState = {n:state.n - action.payload}
+            return newState
+        }
+        else {
+            return state
+        }
+    }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+    return newState//這有啥用啊？
+}
+
+const store = createStore(stateChanger)
+
+
+
+
+ReactDOM.render(
+  <Provider store={store}> 
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
+
 serviceWorker.unregister();
